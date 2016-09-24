c6a8e81edc2820f8d73f06dceb09072aaeabbb10
Julie S Mike <juliesmike@gmail.com>
2016-09-24 12:43:25 -0500

    Add all input fields, create some formating, create initial table

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# Base Project
+# Weekend Challenge #1
 
-1. index.html document
-  - Main content of the web page
-  - html
-  - Links to js and css
-2. apps.js document
-  - Functionality for my site
-  - Javascript
-3. stylesheet.js document
-  - Styles for my html document
-  - css
+- Create an application that records employee data, including their salary. We also want to add the salaries up so we know how much we’re spending each month.
+
+The application should first have an input form that collects:
+
+- The Employee's First and Last name
+- The Employee's ID Number
+- The Employee's Job Title
+- The Employee's Salary (Yearly)
+- The form should have a submit button that
+clears out the form and
+stores the submitted information.
+Then, that information should be appended to the DOM so the user of the application can see the information they just entered.
+
+- Finally, your logic should calculate all of the employee salaries and report back what the monthly cost of salaries is.
--- apps.js
+++ apps.js
@@ -1,3 +1,64 @@
-//Created September 20, 2016 by Julie S. Mike
+//Created September 23, 2016 by Julie S. Mike
 
-console.log("Hello world!");
+//FORMS
+$(document).ready(function(){
+  //event listener
+  $('#employee-info').on('submit', function(event){
+  //to prevent default behavior of adding name labels
+  event.preventDefault();
+
+  //variable that takes in employee as object
+  var employee = {};
+
+  //method serialize array to take values from form and put into array
+  //creates [{name: "employeeFirstName"  (this is the element.name)
+  //            value: "Name"}]
+  var fields = $('#employee-info').serializeArray();
+ //console.log('fields', fields);
+
+//put them into object
+  fields.forEach(function(element, index){
+    //first input from form, iterates over them to create object
+    employee[element.name] = element.value;
+
+
+  });
+  console.log('employee object', employee);
+
+  //clear form, .val('changestext')
+  $('#employee-info').find('input[type=text]').val('');
+
+  //appending to the document, call the function creating later
+  appendDom(employee);
+
+  //call my new function that calulates monthlySalary
+  salaryCalc(employee);
+
+  console.log(employeeSalary.value);
+
+  });
+  function appendDom(emp) {
+
+      //employee object is represented by emp, created jQuery object class of employee
+      var $emp =$('<div class="employee"></div>');
+      //appended paragraph to that div employee
+      $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + ' ' + emp.employeeId + ' ' + emp.jobTitle + ' ' + emp.employeeSalary + '</p>');
+      //select something on DOM to append it to
+      $('#employee-data').append($emp);
+  }
+//new function to figure out salary calc
+
+
+
+function salaryCalc(){
+//take in an employee (yearly) salary and you will return a total monthly salary (for all employees).
+var annualSalary = 0;
+var monthlySalary = 0;
+
+
+
+}
+
+
+
+});
