722dd65cdc6756801c90e39bd9a88d1bdb35da09
Julie S Mike <juliesmike@gmail.com>
2016-09-26 12:50:34 -0500

    Style changes made, monthly salary changed to rounded number

--- apps.js
+++ apps.js
@@ -1,8 +1,9 @@
-//Created September 23, 2016 by Julie S. Mike
+//Created September 24, 2016 by Julie S. Mike
 
-//FORMS
+//
 $(document).ready(function(){
 
+  //variable for adding salary and deleting salary
   var totalSalary = 0;
   //event listener
   $('#employee-info').on('submit', function(event){
@@ -22,53 +23,61 @@ $(document).ready(function(){
     employee[element.name] = element.value;
 
   });
-  //console.log('employee object', employee);
 
-  //clear form, .val('changestext')
-  $('#employee-info').find('input[type=text]').val('');
+  //clear form, .val('changestext' and number) had to add both to clear all
+  $('#employee-info').find('input[type=number], [type=text]').val('');
+
 
   //appending to the document, call the function creating later
   appendDom(employee);
-
 
   });
   function appendDom(emp) {
 
       //employee object is represented by emp, created jQuery object class of employee
       var $emp =$('<tr class="employee"></tr>');
-      //appended paragraph to that div employee
-      // orginal    $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + ' ' + emp.employeeId + ' ' + emp.jobTitle + ' ' + emp.employeeSalary + '</p>');
 
-      //append table
-      $emp.append('<td class="employee-columns">' + emp.employeeFirstName +  '</td>');
-      $emp.append('<td class="employee-columns">' + emp.employeeLastName + '</td>');
-      $emp.append('<td class="employee-columns">' + emp.employeeId + '</td>');
-      $emp.append('<td class="employee-columns">' + emp.jobTitle + '</td>');
-      $emp.append('<td class="annualSalary">' + emp.employeeSalary + '</td>');
+      //append table. class for salary used
+      $emp.append('<td>' + emp.employeeFirstName +  '</td>');
+      $emp.append('<td>' + emp.employeeLastName + '</td>');
+      $emp.append('<td>' + emp.employeeId + '</td>');
+      $emp.append('<td>' + emp.jobTitle + '</td>');
+      $emp.append('<td>' + emp.employeeSalary + '</td>');
       $emp.append('<td >  <button class="deleteButton"> Delete Employee </button>  </td>');
 
       //select something on DOM to append it to
       $('#employeeTable').append($emp);
 
+      //go to last delete button add salary temp data file, salary data
+      //added here when append, stored on that element on DOM
       $('.deleteButton').last().data("salary", emp.employeeSalary);
         //console.log($('.deleteButton').last().data("salary"));
       //add total salary on each click
       totalSalary += parseInt(emp.employeeSalary);
+      var $monthlySalary = totalSalary/12
       //display monthly salary
+          console.log(typeof(totalSalary));
       $('#monthly-salary').empty();
-      $('#monthly-salary').append(totalSalary/12);
+      $('#monthly-salary').append(($monthlySalary).toFixed(2));
   }
-
 
 //delete table row
 
     $("#employeeTable").on('click','.deleteButton',function(){
+      //goes to data field and grab salary info to delete & recalc.
+      //click event this bound to button
       var recalcSalary = $(this).data("salary");
       totalSalary -= recalcSalary;
-      $('#monthly-salary').text(totalSalary/12);
+
+      $('#monthly-salary').text((totalSalary/12).toFixed(2));
+      //removes table row , td, (the button is wrapped in td, within a tr)
+      //row is what we want to remove
       $(this).parent().parent().remove();
+      //or $(this).closest('tr').remove();   better
 })
 
 
-
 });
+
+//.toLocaleString('en-US', { currency: 'USD', style: 'currency'});
+//.tofixed(2);
